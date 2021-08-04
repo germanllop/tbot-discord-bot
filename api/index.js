037f9628eac7c14f9ce342a1aac1792771d05974
German <germanllop@gmail.com>
2021-08-04 03:35:10 -0500

    verify debug

--- api/index.js
+++ api/index.js
@@ -5,8 +5,14 @@ const client = require('../config/discord')
 router.post('/verifyDiscord',async (req, res)=>{
     const { code, discordName } = req.body
     if(!code || !discordName) return res.sendStatus(400)
+
+    console.log(discordName, code)
+
     const user = client.users.cache.find(u => u.tag === discordName)
-    if(!user) return res.send('nouser')
+    if(!user) {
+        console.log('No user')
+        return res.send('nouser')
+    }
 
     user.send(`Your TBOT Army Verification code is: ${code}`)
     res.send('OK')
